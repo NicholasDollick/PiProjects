dda2394c6ec55e012b20d6c358ed50d86e91701c
NicholasDollick <nidollick@gmail.com>
2018-06-27 22:23:41 +0000

    refactored methods

--- RFID Lock/database.py
+++ RFID Lock/database.py
@@ -42,6 +42,26 @@ def led_notif_flicker():
         GPIO.output(20, True)
         time.sleep(.1)
         GPIO.output(20, False)
+        
+def unlock_cycle():
+    GPIO.output(20, True)
+    time.sleep(.1)
+    GPIO.output(20, False)
+    GPIO.output(16, True)
+    time.sleep(.1)
+    GPIO.output(16, False)
+    GPIO.output(5, True)
+    time.sleep(.1)
+    GPIO.output(5, False)
+    GPIO.output(26, True)
+    time.sleep(.1)
+    GPIO.output(26, False)
+    GPIO.output(4, True)
+    time.sleep(.1)
+    GPIO.output(4, False)
+    GPIO.output(22, True)
+    time.sleep(.1)
+    GPIO.output(22, False)
     
 def led_off():
     GPIO.output(22, False)
@@ -52,13 +72,13 @@ def led_off():
     GPIO.output(20, False)
 
 def listen():
-    flag = True
     auth = []
     auth_ids = curs.execute("SELECT * FROM auth").fetchall()
     
     for id in auth_ids:
         auth.append(id[0])
     
+    print("[*] Scan card")
     try:
         while(True):
             GPIO.output(16, True)
@@ -66,19 +86,21 @@ def listen():
             if(id in auth):
                 GPIO.output(16, False)
                 led_correct()
-                print("Card is authorized")
+                print("[+] Card is authorized")
+                time.sleep(.1)
+                unlock_cycle()
                 break
             else:
                 GPIO.output(16, False)
                 led_incorrect()
-                print("Card is unauthorized")
+                print("[-] Card is unauthorized")
                 GPIO.output(16, True)
     except Exception:
         print ("crash during listen")
 
-def add_auth():
+def update_auth(query):
     admin_ids = curs.execute("SELECT * FROM admin").fetchall()
-    print("Scan card to add")
+    print("[*] Scan card now")
     card_id = 0
     try:
         GPIO.output(20, True)
@@ -89,48 +111,19 @@ def add_auth():
                 led_notif_flicker()
                 GPIO.output(20, True)
                 break
-        print("Scan admin card to confirm action")
+        print("[*] Scan admin card to confirm action")
         while(True):
             admin_id, text = reader.read()
             if(admin_id == admin_ids[0][0]):
                 led_correct()
                 GPIO.output(20, False)
                 print("[+] Approved")
-                #print(card_id)
-                curs.execute("INSERT INTO auth(id) VALUES(?)", (card_id,))
+                curs.execute(query, (card_id,))
                 conn.commit()
                 time.sleep(1)
                 break
     except Exception:
-        print("crash while trying to add card")
-
-def remove_auth():
-    admin_ids = curs.execute("SELECT * FROM admin").fetchall()
-    print("Scan card to remove")
-    card_id = 0
-    try:
-        GPIO.output(20, True)
-        while(True):
-            card_id, text = reader.read()
-            if(card_id != 0):
-                GPIO.output(20, False)
-                led_notif_flicker()
-                GPIO.output(20, True)
-                break
-        print("Scan admin card to confirm action")
-        while(True):
-            admin_id, text = reader.read()
-            if(admin_id == admin_ids[0][0]):
-                led_correct()
-                GPIO.output(20, False)
-                print("[+] Approved")
-                #print(card_id)
-                curs.execute("DELETE FROM auth WHERE id = ?", (card_id,))
-                conn.commit()
-                time.sleep(1)
-                break
-    except Exception:
-        print("crash while trying to remove card")
+        print("crash while trying to update card")
 
 def run():
     action = ""
@@ -147,10 +140,10 @@ def run():
         action = input("root@hotel:~$ ")
         
         if(action == "1"):
-           add_auth()
+           update_auth("INSERT INTO auth(id) VALUES(?)")
         
         if(action == "2"):
-            remove_auth()
+            update_auth("DELETE FROM auth WHERE id = ?")
         
         if(action == "3"):
             listen()
